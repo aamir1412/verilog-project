d53c60f181fd5c70fb696d086478dbe498b43161
aniketbharti <51299179+aniketbharti@users.noreply.github.com>
2022-04-04 17:38:52 -0400

    sign extensiion changes

--- design/Sign_Extension.v
+++ design/Sign_Extension.v
@@ -1,10 +1,9 @@
 
-module Sign_Extension (in_sign, out_sign, clock);
+module Sign_Extension (in_sign, out_sign);
     input [2:0] in_sign;
-    input clock;
     output reg [7:0] out_sign;
-    always @(posedge clock) 
+    always @(*) 
         begin
-            out_sign <= {5'b00000, in_sign};
+            assign out_sign = {5'b0, in_sign};
         end
 endmodule
--- testbench/Sign_Extension_tb.v
+++ testbench/Sign_Extension_tb.v
@@ -3,15 +3,13 @@
 
 module Sign_Extension_tb();
     reg [2:0] in_sign;
-    reg clock;
     wire [7:0] out_sign;
-    Sign_Extension sign_data (.in_sign(in_sign), .out_sign(out_sign), .clock(clock));
+    Sign_Extension sign_data (.in_sign(in_sign), .out_sign(out_sign));
     initial
 		begin
 			$dumpfile("vcd/Sign_Extension_dump.vcd");
 			$dumpvars(0, Sign_Extension_tb);
             #1;
-            clock = 1;
             in_sign = 3'b011;
             #10;		            		          
         end
