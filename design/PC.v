5f3362d8979abf822666307dc16618db0ee53b06
aniketbharti <51299179+aniketbharti@users.noreply.github.com>
2022-04-06 12:32:56 -0400

    pc added again

---
+++ design/PC.v
@@ -0,0 +1,12 @@
+module PC(reset, clock, pcin, pcout);
+	input [7:0] pcin;
+	input reset, clock;
+	output reg[7:0] pcout;
+    always@(posedge clock or posedge reset)
+		begin
+			if(reset) 
+				pcout <=8'd00000000;
+			else 
+				pcout <= pcin;
+		end
+endmodule
